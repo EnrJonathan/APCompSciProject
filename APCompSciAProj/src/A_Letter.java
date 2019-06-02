66f0f6f3c385809b36ebef31921ba10010c93877
Jonathan <Jonathan@mbp-de-maria.attlocal.net>
2019-06-01 19:52:18 -0700

    IM DONE OwO REEEEEEEEEEEEEEEEEEEEEEEEEE UwU

--- APCompSciAProj/src/A_Letter.java
+++ APCompSciAProj/src/A_Letter.java
@@ -8,12 +8,12 @@
  *  			the map design was inspired by Call of Duty Black Ops. Honestly, i encountered roadblocks all throughout the 
  *  			project because of the fact that I'm trying to teach myself a difficult subject at 1 in the morning all on my
  *  			own because i have no partner. In the end, I did learn a lot on how to developed a small game in java. What I
- *  			think makes my game unique is that i combined 2 separate game ideas into 1. Part of my code cam from Notch who
+ *  			think makes my game unique is that i combined 2 separate game ideas into 1. Part of my code came from Notch who
  *  			is the creator of a game called Minecraft. His code gives me 60fps & it also updates my game a couple thousand
  *  			times per second. The majority of my code came from a youtube tutorial by a youtuber named RealTutsGML & he
- *  			tought me many things on making a game. For example, being able to use the keys or the mouse to activate events
- *  			& how to create a window in order to play the game. Sadly, I was on my own in the end but I still gave it my
- *  			bestto finish up as much as I can. My final thoughts are that graphics for java should be tought to students
- *  			because that's what they need in order to create fun little games.
+ *  			taught me many things on making a game. For example, being able to use the keys or the mouse to activate events
+ *  			& how to create a window in order to play the game. I even got the chance to create AI that follows you around the map.
+ *     			Sadly, I was on my own in the end but I still gave it my best to finish up as much as I can. My final thoughts 
+ *     			are that graphics for java should be taught to studentsbecause that's what they need in order to create fun little games.
  * 
  */
